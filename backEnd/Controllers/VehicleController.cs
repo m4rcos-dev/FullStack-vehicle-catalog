3ecf9855eb6b932ac64c9b01323c98e85f07b0be
m4rcos-dev <dev.marcospereira@gmail.com>
2023-01-22 21:35:44 -0300

    :sparkles: feat: create model and controller Vehicle

---
+++ backEnd/Controllers/VehicleController.cs
@@ -0,0 +1,32 @@
+using backEnd.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backEnd.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+
+  public class VehicleController : ControllerBase
+  {
+    private static List<Vehicle> Vehicles()
+    {
+      return new List<Vehicle>{
+        new Vehicle{ Id = 1, Nome = "Jetta", Marca = "Fiat", Modelo = "SPORT", Valor = 59300, Foto = "Foto Jetta"}
+      };
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+      return Ok(Vehicles());
+    }
+
+    [HttpPost]
+    public IActionResult Post(Vehicle vehicle)
+    {
+      var vehicles = Vehicles();
+      vehicles.Add(vehicle);
+      return Ok(vehicles);
+    }
+  }
+}
---
+++ backEnd/Model/Vehicle.cs
@@ -0,0 +1,17 @@
+namespace backEnd.Model
+{
+  public class Vehicle
+  {
+    public int Id { get; set; }
+
+    public string Nome { get; set; }
+
+    public string Marca { get; set; }
+
+    public string Modelo { get; set; }
+
+    public int Valor { get; set; }
+
+    public string Foto { get; set; }
+  }
+}
