02f546318f57a8be07399abf530621e64f97fa97
m4rcos-dev <dev.marcospereira@gmail.com>
2023-01-23 11:05:03 -0300

    :wrench: chore: update migrations populating vehicle table

--- backEnd/Model/Vehicle.cs
+++ backEnd/Model/Vehicle.cs
@@ -2,6 +2,16 @@ namespace backEnd.Model
 {
   public class Vehicle
   {
+    public Vehicle() { }
+    public Vehicle(int id, string nome, string marca, string modelo, int valor, string foto)
+    {
+      this.Id = id;
+      this.Nome = nome;
+      this.Marca = marca;
+      this.Modelo = modelo;
+      this.Valor = valor;
+      this.Foto = foto;
+    }
     public int Id { get; set; }
 
     public string Nome { get; set; }
