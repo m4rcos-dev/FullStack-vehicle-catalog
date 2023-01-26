52f96265413d1913616e215f220a29a81eb4778b
m4rcos-dev <dev.marcospereira@gmail.com>
2023-01-26 10:43:17 -0300

    :sparkles: feat: create a sort function to sort the vehicles

--- frontEnd/src/components/Main.js
+++ frontEnd/src/components/Main.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
-import Login from '../pages/Login';
 
 function Main() {
 
@@ -9,7 +8,6 @@ function Main() {
     <div>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
       </Routes>
     </div>
   )
