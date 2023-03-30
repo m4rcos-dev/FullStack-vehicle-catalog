37ee013aef7e51f19a9b382c0ea7b0d5bf98d99b
m4rcos-dev <dev.marcospereira@gmail.com>
2023-03-29 21:44:48 -0300

    :package: build: add lottieFiles pack to animations

--- frontEnd/src/assets/index.js
+++ frontEnd/src/assets/index.js
@@ -1,2 +1,3 @@
 export { default as karshopBlackLogo } from './karshop_black.svg';
 export { default as karshopWhiteLogo } from './karshop_white.svg';
+export { default as loadingAnimation } from './loadingAnimation.json';
